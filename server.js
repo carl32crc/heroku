ae5484c98705cc69eba82aca2df2425ecda1d09d
Carl Ruiz <carl.45.cr@gmail.com>
2016-08-30 13:20:33 +0200

    prueba heroku2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-let app = express()
-
-app.get('/', (req,res) => {
+app.get('/', function (req,res) {
 	res.send("Welcome to my app");
 })
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+app.listen(PORT, function() {
+	console.log("Listening on port " + PORT)
+});
